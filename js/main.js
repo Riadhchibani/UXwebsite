387311ba21f805c82cefa67f336c5abcddfe1698
Riadhchibani <riadhchibani144@gmail.com>
2023-04-14 01:44:41 +0200

    added machine page and owl plugin not yet working

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 function openNav() {
     document.getElementById("mySidebar").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
@@ -7,3 +8,16 @@ function openNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
   }
+  $(document).ready(function() {
+ 
+    $("#owl-demo").owlCarousel({
+   
+        autoPlay: 3000, //Set AutoPlay to 3 seconds
+   
+        items : 4,
+        itemsDesktop : [1199,3],
+        itemsDesktopSmall : [979,3]
+   
+    });
+   
+   });
